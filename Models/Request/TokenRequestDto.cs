6ae12ad49b6e9a75adff012000baae9c0fc12fc9
Rahul Pandey <rahulpandey862@gmail.com>
2026-01-25 06:34:00 +0530

    Added TokenRequest Dto

---
+++ Models/Request/TokenRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace IntroductionToAPI.Models.Request
+{
+    public class TokenRequestDto
+    {
+        public string UserName { get; set; } = default!;
+
+        public string Password { get; set; } = default!;
+    }
+}
